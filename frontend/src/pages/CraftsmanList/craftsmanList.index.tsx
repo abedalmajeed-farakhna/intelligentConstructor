5c2f96e70627a5f0bb3f716c60c2f242ad69dc6a
Abed-Farakhna <121258389+abedalmajeed-farakhna@users.noreply.github.com>
2023-01-11 21:43:53 +0200

    craftsmane files

---
+++ frontend/src/pages/CraftsmanList/craftsmanList.index.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+const CraftsmanList :React.FC<any>= ({children}) => {
+
+  return (
+   <div> CraftsmanList</div>
+  );
+};
+export default CraftsmanList;
